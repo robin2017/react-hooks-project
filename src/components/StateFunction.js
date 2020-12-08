110ddff565ee50372758c640a52b491b095cdcbe
robin2017 <robin.seu@foxmail.com>
2020-12-09 00:46:52 +0800

    suc

--- src/components/StateFunction.js
+++ src/components/StateFunction.js
@@ -1,14 +1,16 @@
-import React, { useState, useMemo, } from 'react'
-
+import React, { useState, useMemo, useCallback} from 'react'
+const set = new Set()
 function StateFunction() {
     const [num, setNum] = useState(1)
     const [age, setAge] = useState(18)
-    const doubleNum = useMemo(() => {
+    const doubleNum = useCallback(() => {
         console.log('获取双倍num')
         return 2 * num
     }, [num])
+    set.add(doubleNum)
+    console.log('set的size：',set.size)
     return <div onClick={() => {
         setAge(age + 1)
-    }}>这是一个函数组件-{doubleNum}</div>
+    }}>这是一个函数组件-{doubleNum()}</div>
 }
 export default StateFunction
