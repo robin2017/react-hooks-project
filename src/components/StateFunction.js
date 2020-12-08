da91226ccab9abd1420fe7edaa16ab834ca7daf1
robin2017 <robin.seu@foxmail.com>
2020-12-08 23:29:50 +0800

    suc

--- src/components/StateFunction.js
+++ src/components/StateFunction.js
@@ -1,20 +1,16 @@
-import React, { useState, useEffect } from 'react'
-const eventHandle = () => { }
+import React, { useState, useEffect, useLayoutEffect } from 'react'
+
 function StateFunction() {
-    const [num, setNum] = useState(0)
-    useEffect(() => {
-        console.log('函数组件结束渲染')
-        document.body.addEventListener('myType', eventHandle)
-        //useeffect第一个参数返回值会在下一次副作用调用前执行
-        //常见场景：事件销毁
-        return () => {
-            document.body.removeEventListener('myType', eventHandle)
-            console.log('销毁上一次')
+    const [greeting, setGreeting] = useState('你好')
+    //useEffect会有闪屏，useLayoutEffect不会
+    useLayoutEffect(() => {
+        console.log('问候语:', greeting)
+        if (greeting === '内心草泥马') {
+            setGreeting('你好')
         }
-    }, [num])
+    }, [greeting])
     return <div onClick={() => {
-        //setState参数除了数值外，也可以为函数
-        setNum(num => num + 1)
-    }}>这是一个函数组件-{num}</div>
+        setGreeting('内心草泥马')
+    }}>这是一个函数组件-{greeting}</div>
 }
 export default StateFunction
